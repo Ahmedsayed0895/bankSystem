806bc8def6be60bdba754dd1215ee7b6065e0ebb
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-07-27 18:51:16 +0300

    add:entities

---
+++ src/main/kotlin/entity/Admin.kt
@@ -0,0 +1,8 @@
+package org.example.entity
+
+data class Admin(
+    val id: Int,
+    val name: String,
+    val password: String,
+    val salary: Double,
+)
---
+++ src/main/kotlin/entity/Client.kt
@@ -0,0 +1,8 @@
+package org.example.entity
+
+data class Client(
+    val id: Int,
+    val name: String,
+    val password: String,
+    val balance: Double,
+)
